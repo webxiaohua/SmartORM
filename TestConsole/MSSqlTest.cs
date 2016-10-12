c3d5d90c5b8867cc2b261b400f955845db5372cb
sxh沈新华 <shen_xh@ctrip.com>
2016-10-12 14:56:20 +0800

    xxx

--- TestConsole/MSSqlTest.cs
+++ TestConsole/MSSqlTest.cs
@@ -7,7 +7,7 @@ namespace TestConsole
 {
     public class MSSqlTest
     {
-        string connString = "Data Source=s2.1000da.com.cn; Initial Catalog=qianlidatest; User ID=qianlida; Password=qianlida123;";
-
+        //string connString = "Data Source=s2.1000da.com.cn; Initial Catalog=qianlidatest; User ID=qianlida; Password=qianlida123;";
+        //string connString = "Data Source=.;Initial Catalog=messageDB;Integrated Security=True";
     }
 }

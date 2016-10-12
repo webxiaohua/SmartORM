7eb05820b8605bb3ce7938c0d432552067982bc8
sxh沈新华 <shen_xh@ctrip.com>
2016-10-12 14:40:31 +0800

    xx

---
+++ TestConsole/MSSqlTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsole
+{
+    public class MSSqlTest
+    {
+        string connString = "Data Source=s2.1000da.com.cn; Initial Catalog=qianlidatest; User ID=qianlida; Password=qianlida123;";
+
+    }
+}
